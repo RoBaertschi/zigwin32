379b5c0221a7349909e2cc1d217256f172081623
RoBaertschi <rtmbaertschi007@gmail.com>
2024-04-06 14:22:00 +0200

    Add new build system

--- build.zig
+++ build.zig
@@ -4,13 +4,13 @@ const std = @import("std");
 const is_zig_0_11 = std.mem.eql(u8, builtin.zig_version_string, "0.11.0");
 
 pub fn build(b: *std.Build) void {
-    if (is_zig_0_11) {
-        _ = b.addModule("zigwin32", .{
-            .source_file = .{ .path = "win32.zig" },
-        });
-    } else {
-        _ = b.addModule("zigwin32", .{
-            .root_source_file = .{ .path = "win32.zig" },
-        });
-    }
+    const optimize = b.standardOptimizeOption(.{});
+    const target = b.standardTargetOptions(.{});
+
+    _ = b.addModule("zigwin32", .{
+        .root_source_file = .{ .path = "win32.zig" },
+        .optimize = optimize,
+        .target = target,
+        .link_libc = true,
+    });
 }
